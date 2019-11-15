7fcfcd6a45a3eabd8f246c497aead55c90f608d2
Sofia Sanchez Fuentes <sofia.sanchezf@estudiante.uam.es>
2019-11-15 18:21:05 +0100

    he empezado el algoritmo. He empezado con la primera parte

--- Práctica 1/transforma.c
+++ Práctica 1/transforma.c
@@ -15,14 +15,19 @@ typedef struct{
 
 
 /*
-*
+* Funcion que crea una transicion
 *
 *
 */
-Transicion *crear_transicion(num_estados) {
+Transicion *crear_transicion(int num_estados, char* simbolo, char* final) {
   Transicion *transicion = NULL;
   int i;
 
+  strcpy(transicion->simbolo, simbolo);
+  strpcy(transicion->estado_final,final);
+
+  //ESTO NO SE MUY BIEN SI ES ASI PORQUE IGUAL LO QUE TENGO QUE HACER ES PASARLE LA LISTA ENETERA NO ???? 
+  // ********* DUDA ************** PERO SI YA LA TENGO EN INTERMEDIA PARA QUE LA QUIERO AQUI ???? --> no me acuerdo para que era 
   transicion->t_codificacion = (int*)malloc(sizeof(int)*num_estados);
   for (i = 0; i < num_estados; i++) {
     transicion->t_codificacion[i] = 0;
@@ -33,15 +38,35 @@ Transicion *crear_transicion(num_estados) {
 }
 
 /*
+* Funcion que elimina una transicion
 *
+*/
+
+void eliminar_transicion(Transicion *transicion) {
+
+  free(transicion->t_codificacion);
+  free(transicion);
+
+}
+
+
+/*
+* Funcion que crea una estructura intermedia
 *
 *
 */
-Intermedia *crear_intermedia(nombre, num_estados, tipo) {
+Intermedia *crear_intermedia(char* nombre, int num_estados, int tipo) {
   Intermedia *intermedia = NULL;
   int i;
 
+  intermedia = (Intermedia *)malloc(sizeof(Intermedia));
+  if (!intermedia) {
+    print("Error al reservar memoria en intermedia\n");
+    return NULL;
+  }
+
   intermedia->tipo = tipo;
+
   intermedia->i_codificacion = (int*)malloc(sizeof(int)*num_estados);
 
   //hay que inicializarla toda a 0
@@ -54,22 +79,161 @@ Intermedia *crear_intermedia(nombre, num_estados, tipo) {
   return intermedia;
 }
 
+/*
+* Funcion que elimina una estructura intermedia
+*
+*/
+
+void eliminar_transicion(Intermedia *intermedia) {
+
+  free(intermedia->i_codificacion);
+  free(intermedia);
+
+}
+
+
+  /* ALGORITMO: 
+  Cogemos el primero que hay en la lista de los creados.
+    Desde cada estado de los creados:
+      Para cada simbolo:
+        Para cada subestado:
+          Para cada estado posible si puedo ir con ese estado
+          Si puedo ir con lambda
+            se hace set de la lista [1,0,0,1]
+      Añadimos una nueva transicion
+      Lo añadimos a la lista de estados creados
+    Lo añadimos a la lista de estados expandidos
+    
+    
+    */
+
+
+   /* En creados añadimos el estado inicial
+   c = q0;
+   para q0:
+    Para cada simbolo: - 0:
+                        Para cada sub estado: en este caso solo tenemos q0.
+                        q0 -> q0 [1,0,0,0,0]
+                        q0 -> q1 [0,1,0,0,0]
+                        tenemos que crear un nuevo estado --> hacemos el or de estas dos: [1,1,0,0,0] tenemos q0q1
+                        Añadimos la transicion a ese estado: origen q0 destino q0q1
+                        Añadimos q0q1 a c
+
+                       - 1:
+                        Para cada sub estado: en este caso solo tenemos q0.
+                        q0 -> q0
+
+                        como ya tenemos un estado que se corresponde con q0 no creamos otro
+                        No añadimos transicion porque no hemosc creado
+                        No añadimos q0 a c (puesto que ya lo hemos visto)
+    Añadimos a expandidos
+  Para cada  q0q1 que es el siguiente que hay:
+    Para cada simbolo: - 0:
+                        Para cada sub estado:
+                          * q0: q0,q1-> q0q1[1,1,0,0,0]
+                          * q1: q1 -> x
+                          obtenemos que desde este solo podemos ir a el mismo
+                        - 1:
+                        Para cada sub estado:
+                          * q0: q0 -> q0 
+                          * q1: q1 -> q2
+                          Tenemos que crear un uevo estado --Z hacemos el or de estas dos [1,0,1,0,0] tenemos q0q2
+                          Añadimos la transicion a ese estado: origen, es el mismo, es decir la estructura con la que estamos trabanando
+                          y el destino es q0q2 que lo acabamos de crear.
+                          Añadimos q0a2 a c
+    Añadimos a expandidos
+    Para cada q0q2: (no hemos incluido q0q1 porque ya lo hemos explorado)
+      Para cada simbolo: - 0:
+                        Para cada sub estado:
+                          * q0: q0,q1
+                          * q2: x
+                          Tenemos el estado q0q2 -> q0,q1 que no lo añadimos
+
+                         - 1:
+                         PAra cada sub estado:
+                         * q0: x
+                         * q2: x
+                         Como no se puede ir a nada y q0q2 ya lo tenemos en creados el algoritmo termina y ya lo tenemos todo pensado
+    Añadimos a lista de expandidos
+
+    */
+
+
+   /* !!!!!! DUDAS !!!!!!!
+   - para que queremos la lista expandidos ???
+   - la lista de creados esta bien -> es un array de no sabemos cuanto tamaño que contiene estructuras intermedias que es donde tenemos todas esas estructuras intermedias
+          - en la lista de creados tenemos todos los nuevos estados y estos nuevos estados tienen esta informacion:
+                * son ellos mismos el estado origen
+                * tienen un estado final
+                * tienen con que simbolo van a ir 
+                (cosas a saber de la transicion)
+                * tienen su nombre
+                * tienen su codificacion para poder saber que estado es y si ya esta no lo creamos mas
+                * tienen si son de tipo final o de que tipo --> demomento no entiendo muy bien para que necesitamos esto
+                (cosas a saber del estado)
+
+    - seria util crear una funcion que compare si las codificaciones son iguales? no lo se -> pensarlo, auqnue creo que si puesto
+    que vamos a tener que ver si ya esta creado o no
+    */
+
 
 AFND* AFNDTransforma(AFND* afnd){
 
-  Intermedia *intermedia = NULL;
+  // TENGO UN LIO CON LOS PUNTEROS DE LAS COSAS ESTAS
+  // le he puesto MAX NOMBRE PARA QUE NO ME DIERA ERROR PORQUE SI NO AL HACER creados[contador] = inicial; ME DA ERROR AL ASIGNAR VARIABLES
+  Intermedia *creados[MAX_NOMBRE] = NULL, *inicial, *estado_actual; //creo que aqui vamos a guardar todos los estados que vayan saliendiendo en el afnd
   int num_estados, num_simbolos;
+  Intermedia *expandidos = NULL; //la creo aunque demomento no etiendo para que la quiero
+  int pos_inicial;
+  char* nombre_inicial, *simbolo;
+  int i,i_simbolo,i_subestado, contador = 0; //i me sirve para contar por el estado que voy en la lista creados
+                                 //i_simbolo me sirve para saber porque simbolo voy 
 
   //obtenemos datos del automata original
   num_estados = AFNDNumEstados(afnd);
   num_simbolos = AFNDNumSimbolos(afnd);
 
-  /*
-  Yo se que toda la informacion necesaria la tengo en AFND.
-  Aqui ya tengo las transiciones y los num_simbolos
+  //obtenemos el estado inicial. 
+  //Lo que recibimos es la posicion del estado inicial
+  pos_inicial = AFNDIndiceEstadoInicial(afnd);
+
+  //tenemos que crear la estructura intermedia para el estado incial
+  //obtenemos el nombre del estado incial
+  nombre_inicial = AFNDNombreEstadoEn(afnd, pos_inicial);
+
+  //como soy el inicial y se que posicion tengo tendre que decirle a la i_codificacion que soy el [1,0,0,0,0]
+  inicial = crear_intermedia(nombre_inicial, num_estados, pos_inicial); //a esta funcion se le pasa el tipo pero no se si es necesario --> LE HE PASADO POS_INICIAL PARA QUE NP ME SALGA ERROR TODO EL RATO PERO HAY QUE CAMBIARLO
+  //lo hago aqui cuando ya he creado la estructura intermedia
+  inicial->i_codificacion[pos_inicial] = 1;
+  
+  creados[contador] = inicial;
+  contador ++;
+
+  //para cada estado que tenemos en creados: ---> !! he comparado i < num_estados porque es lo unico que me cuadra pero igual lo que tneemos
+  // que hacer es para cada estado del afnd ??? no se nuestro algoritmo no es asi pero ESTO HAY QUE OENSARLO
+  for (i = contador-1; i < num_estados; i++){
+    estado_actual = creados[i];
+    //ahora queremos para cada simbolo --> tenemos el numero de simbolos
+    for (i_simbolo = 0; i_simbolo < num_simbolos; i_simbolo++){
+      
+      //digo yo que tendremos alguna funcion que me diga a que estados voy con ese simbolo desde el actual ??? --> creo que si que la hay 
+      // lo que tengo que hacer es coger el simbolo del alfabeto
+      strcpy(simbolo, AFNDSimboloEn(afnd, i_simbolo)); //ya tenemos el primer simbolo
+
+      //para cada sub estado del estado actual: Con subestado nos queremos referir a que si el estado actual es q0q1 tenemos el estado incial
+      for(i_subestado = 0; i_subestado < num_estados; i++) {
+        //comprobamos que subestado estamos trabajando mirando en la cadena [0,0,0,0,0,0] que posiciones estan a 1, si esta a 1 --> tenemos un subestado
+        if (estado_actual->i_codificacion[i_subestado] == 1) {
+          //si estamos en un subestado tenemos que mirar a donde podemos ir
+          //sabemos que el estado actual YA ES EN SI EL INICIAL --> en i_subestado tengo la posicion que soy: es decir si soy q0q2 primero tendre i_subestado = 0 y luego i_subestado = 2
 
 
 
+        }
+      }
 
+    }
+
+  }
 
 }
