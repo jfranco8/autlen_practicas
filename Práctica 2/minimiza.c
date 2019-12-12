e92f9bf9c5f4d4143ab3a242863e294e3f1cfc68
Jesus <jesus.franco@estudiante.uam.es>
2019-12-12 20:05:01 +0100

    funciona pero se para :(

--- Práctica 2/minimiza.c
+++ Práctica 2/minimiza.c
@@ -80,13 +80,14 @@ int isDistinguibleRecursiva(AFND* afnd, int **matriz, int pos1, int pos2, int nu
   int i_simbolo;
   int nuevo_pos1, nuevo_pos2;
   int i;
-  int clase[num_estados];
+  /*int clase[num_estados];*/
   int contador1 = 0; /* para saber si es distinguible o no */
   int contador2 = 0; /* para saber si es distinguible o no */
+  int contador = 0, new_pos1, new_pos2;
 
-  for(i=0; i < num_estados; i++){
+  /*for(i=0; i < num_estados; i++){
     clase[i] = 0;
-  }
+  }*/
 
   /*miramos si estan marcados*/
   if (isDistinguible(matriz, pos1, pos2) == 0){
@@ -100,25 +101,67 @@ int isDistinguibleRecursiva(AFND* afnd, int **matriz, int pos1, int pos2, int nu
 
   /* para cada simbolo */
   for (i_simbolo = 0; i_simbolo < num_simbolos; i_simbolo++){
+    new_pos1 = -1;
+    new_pos2 = -1;
     /* para cada estado */
     for(i=0; i < num_estados; i++){
-      if(AFNDTransicionIndicesEstadoiSimboloEstadof(afnd, pos1, i_simbolo, i) == 1){
-        contador1 += isDistinguibleRecursiva(afnd, matriz, pos1,i,num_estados,num_simbolos);
+      if(i!=pos1 && i!=pos2){
+        if(AFNDTransicionIndicesEstadoiSimboloEstadof(afnd, pos1, i_simbolo, i) == 1){
+          new_pos1 = i;
+        /*  printf("hay transicion de %d a %d por el sim %d\n", pos1, i, i_simbolo);
+          contador1 += isDistinguibleRecursiva(afnd, matriz, pos1,i,num_estados,num_simbolos);
+          printf("\t dice que %d y %d es distinguible si 1: RES = %d\n", pos1, i, contador1);*/
+        }
+        if(AFNDTransicionIndicesEstadoiSimboloEstadof(afnd, pos2, i_simbolo, i) == 1){
+          new_pos2 = i;
+        /*  printf("hay transicion de %d a %d por el sim %d\n", pos2, i, i_simbolo);
+          contador2 += isDistinguibleRecursiva(afnd, matriz, pos2,i,num_estados,num_simbolos);
+          printf("\t dice que %d y %d es distinguible si 1: RES = %d\n", pos2, i, contador2);*/
+        }
       }
-      if(AFNDTransicionIndicesEstadoiSimboloEstadof(afnd, pos2, i_simbolo, i) == 1){
-        contador2 += isDistinguibleRecursiva(afnd, matriz, pos2,i,num_estados,num_simbolos);
-      }
+    }
+    if(new_pos1!=-1 && new_pos2!=-1){
+      contador += isDistinguibleRecursiva(afnd, matriz, new_pos1, new_pos2,num_estados,num_simbolos);
     }
   }
 
   /* cuando el contador es mayor que 0 es porque hemos encontrado cosas distingubles */
-  if (contador1 > 0 && contador2 > 0) {
+  /*if (contador1 != 0 && contador2 != 0) {*/
+  if (contador != 0) {
     return 1;
   }
 
   return 0;
 }
 
+void tachaTablaRecursiva(AFND* afnd, int **matriz_distiguibles, int num_estados, int num_simbolos){
+  int flag = 0;
+  int i, j;
+  for (i=0; i < num_estados; i++){
+    for(j=0; j < num_estados; j++){
+      if(i != j){
+        printf("mira isdistinguible (%d, %d)\n", i, j);
+        if (isDistinguibleRecursiva(afnd, matriz_distiguibles, i, j, num_estados, num_simbolos) == 1){
+          if(matriz_distiguibles[i][j] != 1) {
+            printf("tacha el par %d, %d\n", i, j);
+            flag=1;
+          }
+          matriz_distiguibles[i][j] = 1;
+          matriz_distiguibles[j][i] = 1;
+        }
+      }
+    }
+  }
+
+  for (i=0; i < num_estados; i++){
+    for(j=0; j < num_estados; j++){
+      printf("%d-", matriz_distiguibles[i][j]);
+    }
+    printf("\n");
+  }
+
+  if(flag==1) tachaTablaRecursiva(afnd, matriz_distiguibles, num_estados, num_simbolos);
+}
 
 void estadosDistinguibles(AFND *afnd){
   int num_estados, num_simbolos;
@@ -136,6 +179,7 @@ void estadosDistinguibles(AFND *afnd){
       matriz_distiguibles[i][j] = 0;
     }
   }
+  printf("ha reservado memoria para la matriz jeje\n");
 
   /* Hayamos los primeros distinguibles, los que sean finales */
   for (i=0; i < num_estados; i++){
@@ -149,24 +193,36 @@ void estadosDistinguibles(AFND *afnd){
     }
   }
 
-
-  /* para cada p-q*/
   for (i=0; i < num_estados; i++){
     for(j=0; j < num_estados; j++){
+      printf("%d-", matriz_distiguibles[i][j]);
+    }
+    printf("\n");
+  }
+
+
+  /* para cada p-q*/
+  /*for (i=0; i < num_estados; i++){
+    for(j=0; j < num_estados; j++){
       if(i != j){
+        printf("mira isdistinguible (%d, %d)\n", i, j);
         if (isDistinguibleRecursiva(afnd, matriz_distiguibles, i, j, num_estados, num_simbolos) == 1){
+          if(matriz_distiguibles[i][j] != 1) flag=1;
           matriz_distiguibles[i][j] = 1;
           matriz_distiguibles[j][i] = 1;
         }
       }
     }
-  }
+  }*/
+
+  tachaTablaRecursiva(afnd, matriz_distiguibles, num_estados, num_simbolos);
 
 
   /* imprimimos la matriz */
-  for (i=0; i < num_estados; i++){
+  /*for (i=0; i < num_estados; i++){
     for(j=0; j < num_estados; j++){
-      printf("%d-\n", matriz_distiguibles[i][j]);
+      printf("%d-", matriz_distiguibles[i][j]);
     }
-  }
+    printf("\n");
+  }*/
 }
