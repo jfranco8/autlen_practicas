f41d14c8cfaff6b7c148f96a7db0e6dcbe4b0542
Jesus <jesus.franco@estudiante.uam.es>
2019-12-18 21:11:20 +0100

    entrega lista

--- Práctica 2/prueba_todo_minimiza.sh
+++ Práctica 2/prueba_todo_minimiza.sh
@@ -1,12 +1,12 @@
 make clean
 make
 ./prueba1
-dot -Tpng prueba1.dot > prueba1.png
+dot -Tpng afd.dot > prueba1.png
 
 ./prueba2
-dot -Tpng prueba2.dot > prueba2.png
+dot -Tpng afd.dot > prueba2.png
 
 ./prueba3
-dot -Tpng prueba3.dot > prueba3.png
+dot -Tpng afd.dot > prueba3.png
 
 make clean
